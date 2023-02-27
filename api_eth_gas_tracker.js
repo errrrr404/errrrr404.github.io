8d8a4bc20b8a575650ad1de5647ad7e90a03fae3
Stan Smith <110107791+errrrr404@users.noreply.github.com>
2023-02-27 23:15:07 +0200

    Update api_eth_gas_tracker.js

--- api_eth_gas_tracker.js
+++ api_eth_gas_tracker.js
@@ -3,7 +3,6 @@ function getData() {
   const apiKey = WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ; // здесь нужно вставить ваш API ключ etherscan.io
   const API_URL = 'https://api.etherscan.io/api?module=account&action=balance&address=0x742d35Cc6634C0532925a3b844Bc454e4438f44e&tag=latest&apikey=WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ';
   // используем fetch для получения данных из API
-  console.log("Starting API request");
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
