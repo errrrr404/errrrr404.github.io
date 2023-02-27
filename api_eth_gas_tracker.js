a01329b02e3ce759ad3ccd821209484cf3e31d05
Stan Smith <110107791+errrrr404@users.noreply.github.com>
2023-02-27 23:19:30 +0200

    Update api_eth_gas_tracker.js

--- api_eth_gas_tracker.js
+++ api_eth_gas_tracker.js
@@ -1,19 +1,10 @@
-// создаем функцию для получения данных с API etherscan.io
-function getData() {
-  const apiKey = WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ; // здесь нужно вставить ваш API ключ etherscan.io
-  const API_URL = 'https://api.etherscan.io/api?module=account&action=balance&address=0x742d35Cc6634C0532925a3b844Bc454e4438f44e&tag=latest&apikey=WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ';
-  // используем fetch для получения данных из API
-  
-  fetch(apiUrl)
-  console.log("Starting API request");
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.result); // выводим полученные данные в консоль
-      // здесь вы можете использовать полученные данные для отображения на вашем сайте
-    })
-    .catch(error => console.error(error));
-  console.log(response);
-}
+const apiKey = WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ;
+const apiUrl = `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ}`;
 
-// вызываем функцию для получения данных
-getData();
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const gasPrice = data.result;
+    console.log(`Current gas price: ${gasPrice} wei`);
+  })
+  .catch(error => console.error(error));
