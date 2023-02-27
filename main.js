d1c8edc32842c00dbc730070f35949f1ac460479
Stan Smith <110107791+errrrr404@users.noreply.github.com>
2023-02-28 00:39:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const apiUrl = "https://api.etherscan.io/api";
 const apiKey = "WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ";
-const address = "0x4Fe42dFC900EED12A1cD9060d726f41F4495d970";
+const address = "0xE6b169DEE5941F569B989A3B026982703087cfEb";
 
 const url = `${apiUrl}?module=account&action=balance&address=${address}&tag=latest&apikey=${apiKey}`;
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    const balanceInGwei = ethers.utils.formatUnits(balance.data.result, 'gwei');
-    console.log(`gwei: ${balanceInGwei}`);
+    const balance = data.result;
+    console.log(`Balance: ${balance}`);
   });
