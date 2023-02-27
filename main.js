2832480e686dd930e9bbf73fbd41cdc7fcb900ec
Stan Smith <110107791+errrrr404@users.noreply.github.com>
2023-02-27 23:37:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const apiKey = WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ;
-const apiUrl = `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ}`;
+// создаем функцию для получения данных с API etherscan.io
+function getData() {
+  const apiKey = WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ; // здесь нужно вставить ваш API ключ etherscan.io
+  const apiUrl = `https://api.etherscan.io/api?module=account&action=balance&address=0x742d35Cc6634C0532925a3b844Bc454e4438f44e&tag=latest&apikey=${WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ}`;
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const gasPrice = data.result;
-    console.log(`Current gas price: ${gasPrice} wei`);
-  })
-  .catch(error => console.error(error));
+  // используем fetch для получения данных из API
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.result); // выводим полученные данные в консоль
+      // здесь вы можете использовать полученные данные для отображения на вашем сайте
+    })
+    .catch(error => console.error(error));
+}
+
+// вызываем функцию для получения данных
+getData();
