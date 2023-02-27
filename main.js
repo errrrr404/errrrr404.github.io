170fee2a44ca147ac54195a6dc163d88d431dd1c
Stan Smith <110107791+errrrr404@users.noreply.github.com>
2023-02-28 00:13:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-// создаем функцию для получения данных с API etherscan.io
-function getData() {
-  const apiKey = WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ; // здесь нужно вставить ваш API ключ etherscan.io
-  const apiUrl = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ;
+const apiUrl = "https://api.etherscan.io/api";
+const apiKey = "WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ";
+const address = "0x4Fe42dFC900EED12A1cD9060d726f41F4495d970";
 
-  // используем fetch для получения данных из API
-fetch(apiUrl)
+const url = `${apiUrl}?module=account&action=balance&address=${address}&tag=latest&apikey=${apiKey}`;
+
+fetch(url)
   .then(response => response.json())
   .then(data => {
-    const result = data.result;
-    const labels = result.map(record => record.time);
-    const prices = result.map(record => record.close);
-    drawChart(); // добавить эту строку
-    }
+    const balance = data.result;
+    console.log(`Balance: ${balance}`);
+  });
