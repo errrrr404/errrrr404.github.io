e7a8b8720fdc118ccd57c75f85165e2fc5d6c8db
Stan Smith <110107791+errrrr404@users.noreply.github.com>
2023-02-28 00:27:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const apiUrl = "https://api.etherscan.io/api";
 const apiKey = "WWXFQY5ZXRRUMJ9PRA9TTR7SI7ANGA9JYJ";
-const address = "0x5B136C9966342d23eDa33cf99a576f8C97842994";
+const address = "0x4Fe42dFC900EED12A1cD9060d726f41F4495d970";
 
 const url = `${apiUrl}?module=account&action=balance&address=${address}&tag=latest&apikey=${apiKey}`;
 
